5c5cbfbaeec452b21b96a31f8431a4e1845a308a
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2025-10-23 10:12:23 +0000

    err

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,10 +10,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 	binary_tree_t *tmp;
-        
+
 	if (parent == NULL)
 	return (NULL);
-	
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node || !parent)
 		return (NULL);
